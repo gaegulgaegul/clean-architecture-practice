9095bf33955384f14b3564cd96497823d69ef477
gaegulgaegul <dla9721@gmail.com>
2023-08-02 21:10:00 +0900

    feat 입력 유효성 검증

--- src/main/java/io/gaegul/buckpal/account/application/port/in/SendMoneyCommand.java
+++ src/main/java/io/gaegul/buckpal/account/application/port/in/SendMoneyCommand.java
@@ -1,4 +1,33 @@
 package io.gaegul.buckpal.account.application.port.in;
 
-public class SendMoneyCommand {
+import javax.validation.constraints.NotNull;
+
+import io.gaegul.buckpal.account.domain.Account.AccountId;
+import io.gaegul.buckpal.account.domain.Money;
+import io.gaegul.buckpal.support.SelfValidating;
+import lombok.Getter;
+
+/**
+ * 송금 입력 모델
+ */
+@Getter
+public class SendMoneyCommand extends SelfValidating<SendMoneyCommand> {
+	@NotNull
+	private final AccountId sourceAccountId;
+	@NotNull
+	private final AccountId targetAccountId;
+	@NotNull
+	private final Money money;
+
+	public SendMoneyCommand(AccountId sourceAccountId, AccountId targetAccountId, Money money) {
+		this.sourceAccountId = sourceAccountId;
+		this.targetAccountId = targetAccountId;
+		this.money = money;
+
+		/*
+		 * 데이터 입력 전에 유효성 검증을 수행하면 모두 빈 값으로 확인된다.
+		 * 따라서 데이터 바인딩 후 유효성 검증을 수행한다.
+		 */
+		this.validateSelf();
+	}
 }
---
+++ src/main/java/io/gaegul/buckpal/support/SelfValidating.java
@@ -0,0 +1,32 @@
+package io.gaegul.buckpal.support;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+/**
+ * 유효성 검증 규칙에 위반된 정보가 있는지 확인하는 클래스
+ * @Valid 애너테이션의 동작을 대신 수행한다.
+ * @param <T>
+ */
+public abstract class SelfValidating<T> {
+
+	private Validator validator;
+
+	public SelfValidating() {
+		this.validator = Validation.buildDefaultValidatorFactory().getValidator();
+	}
+
+	/**
+	 * 유효성 검증 규칙에 위반된 정보를 확인한다.
+	 */
+	protected void validateSelf() {
+		final Set<ConstraintViolation<T>> violations = validator.validate((T)this);
+		if (!violations.isEmpty()) {
+			throw new ConstraintViolationException(violations)
+		}
+	}
+}
