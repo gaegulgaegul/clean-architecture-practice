6458d3f7b465e3afc235a99f1b3ee9341af9aee4
gaegulgaegul <dla9721@gmail.com>
2023-08-07 22:02:38 +0900

    refactor import 정리

--- src/main/java/io/gaegul/buckpal/account/adapter/in/web/SendMoneyController.java
+++ src/main/java/io/gaegul/buckpal/account/adapter/in/web/SendMoneyController.java
@@ -1,13 +1,14 @@
 package io.gaegul.buckpal.account.adapter.in.web;
 
-import io.gaegul.buckpal.account.application.port.in.SendMoneyCommand;
-import io.gaegul.buckpal.account.application.port.in.SendMoneyUsecase;
-import io.gaegul.buckpal.account.domain.Account;
-import io.gaegul.buckpal.account.domain.Money;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.gaegul.buckpal.account.application.port.in.SendMoneyCommand;
+import io.gaegul.buckpal.account.application.port.in.SendMoneyUsecase;
+import io.gaegul.buckpal.account.domain.Account.AccountId;
+import io.gaegul.buckpal.account.domain.Money;
+import lombok.RequiredArgsConstructor;
 
 /**
  * SendMoneyController
@@ -26,13 +27,13 @@ class SendMoneyController {
      * @param targetAccountId 수신 계좌 ID
      * @param amount 금액
      */
-    @GetMapping("/accounts/send/{sourceAccountId}/{targetAccountId}/{amount}}")
+    @GetMapping("/accounts/send/{sourceAccountId}/{targetAccountId}/{amount}")
     void sendMoney(@PathVariable("sourceAccountId") Long sourceAccountId,
                    @PathVariable("targetAccountId") Long targetAccountId,
                    @PathVariable("amount") Long amount) {
         final SendMoneyCommand command = new SendMoneyCommand(
-                new Account.AccountId(sourceAccountId),
-                new Account.AccountId(targetAccountId),
+                new AccountId(sourceAccountId),
+                new AccountId(targetAccountId),
                 Money.of(amount)
         );
         sendMoneyUsecase.sendMoney(command);
