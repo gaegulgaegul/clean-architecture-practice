b79c31ae56c6ddc77943112c91ea5a038ff3d9ce
gaegulgaegul <dla9721@gmail.com>
2023-08-08 23:04:12 +0900

    test 송금 유스케이스 테스트 클래스 추가

---
+++ src/test/java/io/gaegul/buckpal/account/application/service/SendMoneyServiceTest.java
@@ -0,0 +1,11 @@
+package io.gaegul.buckpal.account.application.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@DisplayName("SendMoneyService 클래스의")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class SendMoneyServiceTest {
+
+}
